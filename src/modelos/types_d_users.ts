0ca97e98806b996f0c00e235034a64e4256df8db
cbuzzz <sebpinsan12@gmail.com>
2024-12-12 01:05:33 +0100

    Arrerreglados defaults

--- src/modelos/types_d_users.ts
+++ src/modelos/types_d_users.ts
@@ -21,7 +21,6 @@ export interface usersfromDBInterface extends usersInterface {
 }
 
 export const usersSchema = new Schema<usersInterface>({
-    _id: { type: String, required: true, unique: true, default: '' },
     username: { type: String, required: true, unique: true, default: '' },
     name: { type: String, required: true, default: '' },
     mail: { type: String, required: true, default: '' },
